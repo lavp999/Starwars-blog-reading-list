697314f0af6fdf3563d69f5b5b30cb3b017a8ac2
Luis <lavp99@hotmail.com>
2022-11-21 01:02:37 +0100

    iniciado navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+export const Navbar = (props) => {
+	return (<>
+				<div className="justtify-content-cnter">
+					<nav className="navbar navbar-light bg-light mb-3">
+						<Link to="/">
+							<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/694px-Star_Wars_Logo.svg.png" />
+						</Link>
+						<div className="dropdown">
+							<button className="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								{`Favoritos  (${props.lista.length}) `}
+							</button>
+							<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<a className="dropdown-item" href="#">Action</a>
+								<a className="dropdown-item" href="#">Another action</a>
+								<a className="dropdown-item" href="#">Something else here</a>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</>
 	);
 };
--- src/js/layout.js
+++ src/js/layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch, useParams } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
@@ -15,15 +15,16 @@ const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
+	const [lista, setLista] = useState([]);
 
 	return (
-		<div>
+		<div className="container">
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
+					<Navbar lista={lista} setLista={setLista} />
 					<Switch>
 						<Route exact path="/">
-							<Home />
+							<Home  lista={lista} setLista={setLista} />
 						</Route>
 						<Route exact path="/demo">
 							<Demo />
