f12b139a7ec97c0149502319904f048f5104c10c
lavp999 <lavp99@hotmail.com>
2022-11-30 15:54:56 +0000

    No tiene los campos que creimos las listas...

--- src/js/component/cardCharacters.js
+++ src/js/component/cardCharacters.js
@@ -10,12 +10,12 @@ export const CardCharacters = (props) => {
     }
 
     function existeFav(lista, id){
-        
+        /*
         if (Array.isArray(lista))
             console.log("Que tengo en la lista?" , lista.id, lista.find(lf => lf.id == id), lista);
         else
             console.log("No tengo lista", lista);
-        
+        */
         
         return !(Array.isArray(lista) && lista.find(lf => lf.id == id));
     }
@@ -23,12 +23,12 @@ export const CardCharacters = (props) => {
     return (<div key={`div-${personaje.uid}`} className="col-4 card tarjeta">
                 <img src={personaje.image ? personaje.image : imagen} className="card-img-top" alt="" />
                 <div className="card-body">
-                    <h5 className="card-title">{personaje.name}</h5>
+                    <h5 className="card-title">{personaje.name} - {personaje.uid}</h5>
                     <p className="card-text">{`Gender: ${personaje.gender}`}</p>
                     <p className="card-text">{`Hair Color: ${personaje.hair_color}`}</p>
                     <p className="card-text">{`Eye-Color: ${personaje.eye_color}`}</p>
 
-                    <Link to={`/singleCharacter/${personaje.uid}`} key={`link-${personaje.uid}`} className="btn btn-primary">
+                    <Link to={`/singleCharacter/${personaje.uid}`} key={`lnk-${personaje.uid}`} className="btn btn-primary">
                         Learn more!
 					</Link>
 
