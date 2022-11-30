e248353da12907e2b53dec4010464bc1a63ed533
lavp999 <lavp99@hotmail.com>
2022-11-30 09:20:39 +0000

    Añadido el control de no duplicar favorito

--- src/js/component/cardCharacters.js
+++ src/js/component/cardCharacters.js
@@ -5,56 +5,34 @@ export const CardCharacters = (props) => {
     const personaje = props.personaje;
 
     const addFavorit = ()=>{
-        console.log("Mi lista favoritos", props.listaFav, personaje.id, personaje.name );
         props.setListaFav([...props.listaFav, {"id":personaje.id, "name":personaje.name }]);
     }
 
     function existeFav(lista, id){
-        return (Array.isArray(lista) && lista.find((lf) => {lf.id === id}));
+        /*
+        if (Array.isArray(lista))
+            console.log("Que tengo en la lista?" ,  lista.find(lf => lf.id == id), lista);
+        else
+            console.log("No tengo lista", lista);
+        */
+        
+        return !(Array.isArray(lista) && lista.find(lf => lf.id === id));
     }
 
     return (<div key={`div-${personaje.id}`} className="col-4 card tarjeta">
-                <img key={`img-${personaje.id}`} src={imagen} className="card-img-top" alt="" />
-                <div key={`div1-${personaje.id}`} className="card-body">
-                    <h5 key={`h5-${personaje.id}`} className="card-title">{personaje.name}</h5>
-                    <p key={`p1-${personaje.id}`} className="card-text">{`Gender: ${personaje.gender}`}</p>
-                    <p key={`p2-${personaje.id}`} className="card-text">{`Hair Color: ${personaje.hair_color}`}</p>
-                    <p key={`p3-${personaje.id}`} className="card-text">{`Eye-Color: ${personaje.eye_color}`}</p>
-                    <a key={`a-${personaje.id}`} href="#" className="btn btn-primary">Learn more!</a>
-                    <button key={`btn-${personaje.id}`}
-                            onClick={()=>addFavorit()}                                 
-                            className="btn btn-primary"><i className="fad fa-heart"></i>
-                    </button>
+                <img src={imagen} className="card-img-top" alt="" />
+                <div className="card-body">
+                    <h5 className="card-title">{personaje.name}</h5>
+                    <p className="card-text">{`Gender: ${personaje.gender}`}</p>
+                    <p className="card-text">{`Hair Color: ${personaje.hair_color}`}</p>
+                    <p className="card-text">{`Eye-Color: ${personaje.eye_color}`}</p>
+                    <a href="#" className="btn btn-primary">Learn more!</a>
 
-           {existeFav(props.ListaFav, personaje.id) && (<button key={`btn1-${personaje.id}`}
-                                                                   onClick={()=>addFavorit()}                                 
-                                                                   className="btn btn-primary"><i className="fad fa-heart"></i>
-                                                           </button>)
+                    {existeFav(props.listaFav, personaje.id) && (<button onClick={()=>addFavorit()}                                 
+                                                                         className="btn btn-primary"><i className="fad fa-heart"></i>
+                                                                 </button>)
                     }
-
-
-
                 </div>
             </div>)
 
 };
-/**
- * <div className="col-4 card tarjeta">
-            <img src={imagen} className="card-img-top" alt="" />
-            <div className="card-body">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">{`Gender: ${props.gender}`}</p>
-                <p className="card-text">{`Hair Color: ${props.hair-color}`}</p>
-                <p className="card-text">{`Eye-Color: ${props.eye-color}`}</p>
-                <a href="#" className="btn btn-primary">Learn more!</a>
-                <a href="#" className="btn btn-primary"><i class="fad fa-heart"></i></a>
-            </div>
-        </div>
-            {props.ListaFav.find((lf) => {lf.id === id}) && <button key={`btn1-${personaje.id}`}
-                                                                   onClick={()=>addFavorit()}                                 
-                                                                   className="btn btn-primary"><i className="fad fa-heart"></i>
-                                                           </button>
-                    }
-
- 
-        */
