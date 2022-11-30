c8e4cddb50b181f623bc8ff6c15a645eac6170b5
lavp999 <lavp99@hotmail.com>
2022-11-30 15:04:00 +0000

    muy encaminado!!! :-)

--- src/js/component/cardCharacters.js
+++ src/js/component/cardCharacters.js
@@ -6,33 +6,33 @@ export const CardCharacters = (props) => {
     const personaje = props.personaje;
 
     const addFavorit = ()=>{
-        props.setListaFav([...props.listaFav, {"id":personaje.id, "name":personaje.name }]);
+        props.setListaFav([...props.listaFav, {"id":personaje.uid, "name":personaje.name }]);
     }
 
     function existeFav(lista, id){
-        /*
+        
         if (Array.isArray(lista))
-            console.log("Que tengo en la lista?" ,  lista.find(lf => lf.id == id), lista);
+            console.log("Que tengo en la lista?" , lista.id, lista.find(lf => lf.id == id), lista);
         else
             console.log("No tengo lista", lista);
-        */
         
-        return !(Array.isArray(lista) && lista.find(lf => lf.id === id));
+        
+        return !(Array.isArray(lista) && lista.find(lf => lf.id == id));
     }
 
-    return (<div key={`div-${personaje.id}`} className="col-4 card tarjeta">
-                <img src={personaje.image} className="card-img-top" alt="" />
+    return (<div key={`div-${personaje.uid}`} className="col-4 card tarjeta">
+                <img src={personaje.image ? personaje.image : imagen} className="card-img-top" alt="" />
                 <div className="card-body">
                     <h5 className="card-title">{personaje.name}</h5>
                     <p className="card-text">{`Gender: ${personaje.gender}`}</p>
                     <p className="card-text">{`Hair Color: ${personaje.hair_color}`}</p>
                     <p className="card-text">{`Eye-Color: ${personaje.eye_color}`}</p>
 
-                    <Link to={`/singleCharacter/${personaje.id}`} key={`link-${personaje.id}`} className="btn btn-primary">
+                    <Link to={`/singleCharacter/${personaje.uid}`} key={`link-${personaje.uid}`} className="btn btn-primary">
                         Learn more!
 					</Link>
 
-                    {existeFav(props.listaFav, personaje.id) && (<button onClick={()=>addFavorit()}                                 
+                    {existeFav(props.listaFav, personaje.uid) && (<button onClick={()=>addFavorit()}                                 
                                                                          className="btn btn-primary"><i className="fad fa-heart"></i>
                                                                  </button>)
                     }
