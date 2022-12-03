6b3c8b71ce6d82daaf2284d55b991fb002b1fb71
Luis <lavp99@hotmail.com>
2022-12-03 23:22:46 +0100

    queda Paginar, imágenes y maquetar mejor

--- src/js/component/cardCharacters.js
+++ src/js/component/cardCharacters.js
@@ -1,5 +1,5 @@
 import React, { useContext }from "react";
-import imagen from "../../img/rigo-baby.jpg"; // https://bloygo.yoigo.com/guia-personajes-star-wars-luke-skywalker-rey/
+import imagen from "../../img/rigo-baby.jpg";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
@@ -8,20 +8,21 @@ export const CardCharacters = (props) => {
     const personaje = props.personaje;
     const {store, actions} = useContext(Context);
 
-    function existeFav(lista, id){
+    function noExisteFav(lista, id, tipo){
         /*
         if (Array.isArray(lista))
             console.log("Que tengo en la lista?" , lista.id, lista.find(lf => lf.id == id), lista);
         else
             console.log("No tengo lista", lista);
         */
-        
-        return !(Array.isArray(lista) && lista.find(lf => lf.id == id));
+
+        return !(Array.isArray(lista) && lista.find(lf => (lf.id == id && lf.tipo == tipo)));
     }
 
-    return (<div key={`div-${personaje.uid}`} className="col-4 card tarjeta">
-                {/* <img src={`../../src/img/fotos/${personaje.uid}.jpg`} className="card-img-top" alt="" /> */}
-                <img key={`img-${personaje.uid}`} src={imagen} className="card-img-top" alt="" /> 
+    return (<div key={`divC-${personaje.uid}`} className="col-4 card tarjeta">
+                {/* <img src={`/../../img/fotos/${personaje.uid}.jpg`} className="card-img-top" alt="" /> */}
+                {/* <img key={`img-${personaje.uid}`} src={imagen} className="card-img-top" alt="" />  */}
+                <img key={`img-${personaje.uid}`} src="https://www.commonsensemedia.org/sites/default/files/styles/ratio_16_9_large/public/blog/starwars-darth-hand-blog-569x329.jpg" className="card-img-top" alt="" /> 
                 <div key={`div2-${personaje.uid}`} className="card-body">
                     <h5 className="card-title">{personaje.name} - {personaje.uid}</h5>
                     <div key={`div3-${personaje.uid}`} className="row p-0 justify-content-between">
@@ -31,13 +32,13 @@ export const CardCharacters = (props) => {
                             </Link>
                         </div>
                         <div key={`div4-${personaje.uid}`} className="col-auto">
-                            {existeFav(store.misFav, personaje.uid) && (<button key={`btn-${personaje.uid}`} onClick={()=>actions.addFav({"id":personaje.uid, "name":personaje.name })}                                 
-                                                                                className="btn btn-primary"><i className="fad fa-heart"></i>
-                                                                        </button>)
+                            {noExisteFav(store.misFav, personaje.uid, "C") && 
+                                            (<button key={`btn-${personaje.uid}`} onClick={()=>actions.addFav({"id":personaje.uid, "name":personaje.name, "tipo": "C" })}                                 
+                                                className="btn btn-primary"><i className="fas fa-heart"></i>
+                                            </button>)
                             }
                         </div>
                     </div>
                 </div>
             </div>)
-
 };
